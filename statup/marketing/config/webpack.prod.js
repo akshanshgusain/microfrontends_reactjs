a750af0ea316f63eddaa491bb26d24929fd73ff4
akshanshgusain <akshanshgusain@gmail.com>
2022-10-14 14:21:47 +0530

    feat: added marketing deployment

--- statup/marketing/config/webpack.prod.js
+++ statup/marketing/config/webpack.prod.js
@@ -9,6 +9,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/marketing/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
