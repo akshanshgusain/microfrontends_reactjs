70218edf67d4e01ec398c38406220f36a9d5ca19
akshanshgusain <akshanshgusain@gmail.com>
2022-10-14 17:22:00 +0530

    feat(statup): added random css classnames

--- statup/container/src/App.js
+++ statup/container/src/App.js
@@ -2,15 +2,25 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ca",
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <h1>Hi There!!! I am Container</h1>
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <h1>Hi There!!! I am Container</h1>
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
